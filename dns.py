5e1da0a5aca8c9a24836a07d67b30fe9874feca5
Imamov_FN <Imamov_FN@surgutneftegas.ru>
2022-05-20 17:48:28 +0500

    work only for one page

--- dns.py
+++ dns.py
@@ -1,10 +1,11 @@
-from doctest import OPTIONFLAGS_BY_NAME
-from filecmp import clear_cache
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+
+import json
 
 
 def close_popups(wait):
@@ -16,8 +17,7 @@ def close_popups(wait):
     build_mode_popup.click()
 
 
-
-url = "https://www.dns-shop.ru/catalog/17a89aab16404e77/videokarty/?from_search=1&order=6&stock=now-today-tomorrow-later-out_of_stock"
+url = "https://www.dns-shop.ru/catalog/17a89aab16404e77/videokarty/?f[mx]=2fi-2fh-dbbc"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--ignore-certificate-errors-spki-list')
@@ -25,29 +25,40 @@ chrome_options.add_argument('log-level=3')
 # chrome_options.add_argument("headless") нужно ещё разобраться
 
 driver = webdriver.Chrome(options=chrome_options)
-wait = WebDriverWait(driver, 3)
+wait = WebDriverWait(driver, 7)
 actions = ActionChains(driver)
-
 driver.get(url)
 
 page = driver
 data_results = []
 
-bottom_navbar_offset = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'bottom-navbar-mobile__btn'))).rect['height']
+bottom_navbar_offset = wait.until(EC.presence_of_element_located(
+    (By.CLASS_NAME, 'bottom-navbar-mobile__btn'))).rect['height']
+original_window = driver.current_window_handle
+
 
 while True:
     elems = wait.until(EC.presence_of_all_elements_located(
-        (By.CSS_SELECTOR, '[data-id="product"]')))
+        (By.CLASS_NAME, "catalog-product__name")))
     for elem in elems:
         elem_data = {}
-        elem_url = elem.find_element(By.CLASS_NAME, "catalog-product__name").get_attribute('href')
-        driver.get(elem_url)
+        elem_url = elem.get_attribute('href')
+        elem.send_keys(Keys.CONTROL + Keys.ENTER)
+        # actions.click(elem)
+        wait.until(EC.number_of_windows_to_be(2))
+
+        for window_handle in driver.window_handles:
+            if window_handle != original_window:
+                driver.switch_to.window(window_handle)
+                break
+
         data_from_elem = {'ulr': elem_url, 'price': wait.until(EC.presence_of_element_located((
             By.CLASS_NAME, 'product-buy__price'))).text}
 
         button_options_expand = wait.until(EC.presence_of_element_located(
             (By.CLASS_NAME, 'product-characteristics__expand')))
-        actions.move_to_element_with_offset(button_options_expand, 0, bottom_navbar_offset)
+        actions.move_to_element_with_offset(
+            button_options_expand, 0, bottom_navbar_offset)
         actions.click()
 
         options = wait.until(EC.presence_of_all_elements_located(
@@ -61,13 +72,18 @@ while True:
             elem_data[name] = value
         data_results.append(elem_data)
 
-        driver.back()
+        driver.close()
+        driver.switch_to.window(original_window)
 
-    next_page = wait.until(EC.presence_of_element_located(
-        (By.CLASS_NAME, 'pagination-widget__page-link_next')))
+    next_page = driver.find_element(
+        By.CLASS_NAME, 'pagination-widget__page-link_next')
     if 'pagination-widget__page-link_disabled' in next_page.get_attribute("class"):
         break
-    
+
+    actions.move_to_element_with_offset(
+        next_page, 0, bottom_navbar_offset)
+    actions.click()
+
 with open('result.txt', 'w') as f:
-    f.write(data_results)
+    f.write(json.dumps(data_results))
 driver.close()
